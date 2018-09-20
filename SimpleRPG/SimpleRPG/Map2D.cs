bfa4a0e67211ae73f49686552e9d9538f7a90a7f
Ernesto Campese <mr.ernestocampese@gmail.com>
2018-09-20 20:50:04 +0100

    Map2D and Program updated.

---
+++ SimpleRPG/SimpleRPG/Map2D.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace SimpleRPG
+{
+
+    class Map2D
+    {
+        private string _name;
+        private readonly int _rows;
+        private readonly int _columns;
+        private int _lastX;
+        private int _lastY;
+
+        private readonly int[,] _map2D;
+
+        public Map2D(int size, string name)
+        {
+            _name = name;
+            _rows = size;
+            _columns = size;
+
+            _map2D = new int[size, size];
+        }
+
+        public int GetPosition(int x, int y)
+        {
+            return _map2D[x, y];
+        }
+
+        public void SetPlayerPosition(int x, int y)
+        {
+            if (x > _rows || y > _columns)
+            {
+                Console.WriteLine("Error: The position doesn't exist.");
+            }
+            else
+            {
+                _map2D[_lastX, _lastY] = 0;
+                _map2D[x, y] = 1;
+                _lastX = x;
+                _lastY = y;
+            }
+        }
+
+        public void Show()
+        {
+            // Rows
+            for (var i = 0; i < _columns; i++)
+            {
+                Console.Write("[{0}]", _map2D[i, 0]);
+
+                // Columns
+                for (var j = 1; j < _rows; j++)
+                {
+                    Console.Write("[{0}]", _map2D[i, j]);
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
--- SimpleRPG/SimpleRPG/Program.cs
+++ SimpleRPG/SimpleRPG/Program.cs
@@ -10,6 +10,45 @@ namespace SimpleRPG
     {
         static void Main(string[] args)
         {
+            Map2D traverseTown = new Map2D(6, "Traverse Town");
+
+            int playerX = 0;
+            int playerY = 0;
+
+            traverseTown.SetPlayerPosition(playerX, playerY);
+
+
+            while (true)
+            {
+                traverseTown.Show();
+                Console.WriteLine();
+                Console.WriteLine("Where you want to go? (WASD): ");
+
+                var input = Convert.ToChar(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 'w':
+                        playerX--;
+                        break;
+                    case 's':
+                        playerX++;
+                        break;
+                    case 'a':
+                        playerY--;
+                        break;
+                    case 'd':
+                        playerY++;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown Command!");
+                        break;
+                }
+
+                traverseTown.SetPlayerPosition(playerX, playerY);
+                Console.Clear();
+            }
+
         }
     }
 }
